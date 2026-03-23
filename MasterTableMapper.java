07bb958f92df3b425e44d13e9a3010aa715fd602
c4pu7 <75448445+c4pu7@users.noreply.github.com>
2026-03-23 10:02:43 +0100

    Update MasterTableMapper.java

--- MasterTableMapper.java
+++ MasterTableMapper.java
@@ -1,12 +1,4 @@
-package eu.unicredit.xframe.qrh.admin.rs.mapper;
 
-import eu.unicredit.qrh.admin.model.entity.MasterTableEntity;
-import eu.unicredit.qrh.admin.dto.MasterTableResponseDTO;
-import eu.unicredit.qrh.admin.dto.UpdateRequestDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-
-import java.time.LocalDateTime;
 
 @Mapper(componentModel = "spring", imports = {LocalDateTime.class})
 public interface MasterTableMapper {
