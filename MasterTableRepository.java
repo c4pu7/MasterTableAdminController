12f276cacdcd21bacdd70cfd18ea6cb34f307f91
c4pu7 <75448445+c4pu7@users.noreply.github.com>
2026-03-23 10:02:56 +0100

    Update MasterTableRepository.java

--- MasterTableRepository.java
+++ MasterTableRepository.java
@@ -1,10 +1,4 @@
-package eu.unicredit.xframe.qrh.admin.rs.repository;
 
-import eu.unicredit.qrh.admin.model.entity.MasterTableEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 @Repository(MasterTableRepository.REPOSITORY_NAME)
 public interface MasterTableRepository  extends JpaRepository<MasterTableEntity, String> {
