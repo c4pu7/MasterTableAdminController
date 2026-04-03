30afee26e6e959fef071417925dbe5869765c026
c4pu7 <75448445+c4pu7@users.noreply.github.com>
2026-04-03 11:29:48 +0200

    Update BaseContextTest.java

--- BaseContextTest.java
+++ BaseContextTest.java
@@ -1,15 +1,24 @@
 
+import eu.unicredit.qrh.admin.lib.configuration.QrhJpaConfig;
 import eu.unicredit.qrh.core.env.service.EnvService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ContextConfiguration(classes = QrhAdminApplication.class)
 @Import(CoreEngineMockConfig.class)
+@ComponentScan(
+        basePackages = "eu.unicredit",
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = QrhJpaConfig.class
+        )
+)
 public class BaseContextTest extends CommonEnvironmentConfig {
 
     @MockBean
     protected EnvService envService;
-}
